1d289b56b2425d8736f54cebce6ae1798f41e60c
Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2021-11-02 13:35:49 +0530

    Add files via upload

---
+++ SerenaRobot/modules/tools.py
@@ -0,0 +1,22 @@
+__mod_name__ = "TOOLS🛠️"
+
+__help__ = """
+*Fake Info*
+ ❦ /fakegen*:* Generates Fake Information
+ ❦ /picgen*:* Generate a fake pic 
+
+*Date-time-Weather*
+ ❦ /time <country code>*:* Gives information about a timezone.
+ ❦ /weather <city>*:* Get weather info in a particular place.
+ ❦ /wttr <city>*:* Advanced weather module, usage same as /weather
+ ❦ /wttr moon*:* Get the current status of moon
+ 
+*Quotly*
+❦ /q : reply any message with this command to create a sticker.
+
+*Converts*
+ ❦ /encrypt*:* Encrypts The Given Text
+ ❦ /decrypt*:* Decrypts Previously Ecrypted Text
+ ❦ /zip*:* reply to a telegram file to compress it in .zip format
+ ❦ /unzip*:* reply to a telegram file to decompress it from the .zip format
+"""
