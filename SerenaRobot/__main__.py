c3eadc552f46f51226a9ecae8114f2eeeeee35b0
Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2021-11-03 15:21:13 +0530

    Update __main__.py

--- SerenaRobot/__main__.py
+++ SerenaRobot/__main__.py
@@ -338,7 +338,7 @@ def help_button(update, context):
                 parse_mode=ParseMode.MARKDOWN,
                 disable_web_page_preview=True,
                 reply_markup=InlineKeyboardMarkup(
-                    [[InlineKeyboardButton(text="Back", callback_data="help_back")]]
+                    [[InlineKeyboardButton(text="[► Back ◄]", callback_data="help_back")]]
                 ),
             )
 
