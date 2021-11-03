6e9ea0227bb310038b6d537e3aded72fc6ca990b
Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2021-11-03 20:43:47 +0530

    Update __main__.py

--- SerenaRobot/__main__.py
+++ SerenaRobot/__main__.py
@@ -797,7 +797,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "I Aᴍ Aʟɪᴠᴇ 🔥")
+            dispatcher.bot.sendMessage(f"@AshSerenaFans", "I Aᴍ Aʟɪᴠᴇ 💖")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"
