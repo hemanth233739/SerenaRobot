8525c52e5ce5194df35a3409de34bda95826349a
Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2021-11-03 16:21:30 +0530

    Update __main__.py

--- SerenaRobot/__main__.py
+++ SerenaRobot/__main__.py
@@ -30,6 +30,7 @@ from SerenaRobot import (
 from SerenaRobot.modules import ALL_MODULES
 from SerenaRobot.modules.helper_funcs.chat_status import is_user_admin
 from SerenaRobot.modules.helper_funcs.misc import paginate_modules
+from SerenaRobot.modules.helper_funcs.alternate import typing_action
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram.error import (
     BadRequest,
