41aabedb5cb9cb3afb62c393dcd4ea6c60f45317
Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2021-12-09 14:56:35 +0530

    Update config.py

--- SerenaRobot/config.py
+++ SerenaRobot/config.py
@@ -1,3 +1,13 @@
+# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
+import json
+import os
+
+
+def get_user_list(config, key):
+    with open('{}/EmiliaAnimeBot/{}'.format(os.getcwd(), config),
+              'r') as json_file:
+        return json.load(json_file)[key]
+
 class Config(object):
     LOGGER = True
     # REQUIRED
